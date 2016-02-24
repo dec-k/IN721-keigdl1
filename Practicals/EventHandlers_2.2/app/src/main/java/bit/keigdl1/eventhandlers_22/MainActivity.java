39de96c2a4c12db711d49842eb440513173d4404
Dec Keighley <keigdl1@student.op.ac.nz>
2016-02-25 12:39:22 +1300

    Practical 2.2 Solution
    Thanks for the help Patricia!

--- Practicals/EventHandlers_2.2/app/src/main/java/bit/keigdl1/eventhandlers_22/MainActivity.java
+++ Practicals/EventHandlers_2.2/app/src/main/java/bit/keigdl1/eventhandlers_22/MainActivity.java
@@ -14,24 +14,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText txtUsername = (EditText) findViewById(R.id.txtUsername);
+        final EditText txtUsername = (EditText) findViewById(R.id.txtUsername);
         txtUsername.setOnKeyListener(new onKeyEventHandler());
-}
+    }
 
     //Inner class for key listener - prompts toast when '@' is typed.
-    public class onKeyEventHandler implements View.OnKeyListener{
+    public class onKeyEventHandler implements View.OnKeyListener {
 
         @Override
         public boolean onKey(View v, int keyCode, KeyEvent event) {
-            if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)){
-                    Toast statusToast = Toast.makeText(MainActivity.this, "Don't Type @!", Toast.LENGTH_SHORT);
+            //Read the contents of the editText & save it into a variable.
+            int viewID = v.getId();
+            EditText etInput = (EditText) findViewById(viewID);
+            String userName = etInput.getText().toString();
+
+            //When enter is pressed, check if username is the appropriate length,
+            //then generate a toast with correct feedback.
+            if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                if(userName.length() == 8){
+                    Toast statusToast = Toast.makeText(MainActivity.this, "Username accepted, " + userName + ".", Toast.LENGTH_LONG);
                     statusToast.show();
+                }else{
+                    Toast statusToast = Toast.makeText(MainActivity.this, "Username must be 8 characters long. Try again.", Toast.LENGTH_LONG);
+                    statusToast.show();
+                }
+
+                //Clears the users entered text for clarify & less confusion.
+                etInput.setText("");
+
+                //Confirms that this onKey event has been handled successfully.
+                return true;
             }
-        return false;
+            return false;
+        }
     }
-}
-    public void test(){
-
-    }
-
 }
