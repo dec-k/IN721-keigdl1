e298820c31ddc5a5485cbe8980cce70400e94b8d
Dec Keighley <keigdl1@student.op.ac.nz>
2016-02-24 16:55:31 +1300

    Practical 2.1 Complete
    If the app doesn't respond to you hitting @, make sure your emulated
    device has Hardware Keyboard enabled - older versions of android studio
    did not turn this on by default.

--- Practicals/EventHandlers_2.1a/app/src/main/java/bit/keigdl1/eventhandlers_21a/MainActivity.java
+++ Practicals/EventHandlers_2.1a/app/src/main/java/bit/keigdl1/eventhandlers_21a/MainActivity.java
@@ -2,8 +2,11 @@ package bit.keigdl1.eventhandlers_21a;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +16,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         EditText txtNoAtSymbol = (EditText) findViewById(R.id.txtNoAtSymbol);
+        txtNoAtSymbol.setOnKeyListener(new onKeyEventHandler());
     }
 
     //Inner class for key listener - prompts toast when '@' is typed.
-    
+    public class onKeyEventHandler implements View.OnKeyListener{
+
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            if (event.getAction() == KeyEvent.ACTION_UP){
+                //Makes this toast only show on a keydown event.
+                if(keyCode == KeyEvent.KEYCODE_AT) {
+                    //A key event is triggered on key up AND down,
+                    // so you'll see two toasts if the event time isn't specified.
+                    Toast statusToast = Toast.makeText(MainActivity.this, "Don't Type @!", Toast.LENGTH_SHORT);
+                    statusToast.show();
+                }
+            }
+
+            return false;
+        }
+    }
 }
