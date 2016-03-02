1ec741fe71ea0ded9a73f295b6e7570a8e307233
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-02 14:52:17 +1300

    ADDED PHASE 1 DOOT

---
+++ Practicals/ComplexHandler_Spinner_2.2/app/src/main/java/bit/keigdl1/complexhandler_spinner_22/SplashAct.java
@@ -0,0 +1,13 @@
+package bit.keigdl1.complexhandler_spinner_22;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
