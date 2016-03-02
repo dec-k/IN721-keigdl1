c5c44a06bec19a1ecdb1801a83984bf48333d2ec
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-02 16:55:56 +1300

    3.2 Required Functionality Added

--- Practicals/ComplexHandler_Spinner_2.2/app/src/main/java/bit/keigdl1/complexhandler_spinner_22/SplashAct.java
+++ Practicals/ComplexHandler_Spinner_2.2/app/src/main/java/bit/keigdl1/complexhandler_spinner_22/SplashAct.java
@@ -1,7 +1,10 @@
 package bit.keigdl1.complexhandler_spinner_22;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashAct extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class SplashAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //Ref for button
+        Button startApp = (Button) findViewById(R.id.btnStart);
+        //Add click listener to button ref
+        startApp.setOnClickListener(new startClickedController());
+    }
+
+    public class startClickedController implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            //Create an intent
+            Intent endSplashScreen = new Intent(SplashAct.this, MainActivity.class);
+            //Change activities by using the intent declared above.
+            startActivity(endSplashScreen);
+        }
     }
 }
