67fe505ff43ab90fe5c02d94b0f94b5ee5cd56eb
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-11 14:30:45 +1300

    Project Creation Commit

---
+++ Practicals/FragmentIntro/app/src/main/java/bit/keigdl1/fragmentintro/MainActivity.java
@@ -0,0 +1,13 @@
+package bit.keigdl1.fragmentintro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
