713664e0361f7fa4ef800b56e7aac5799d0e5065
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-13 10:41:33 +1300

    Successfully Added A fragment changer.

--- Practicals/FragmentsIntro/app/src/main/java/bit/keigdl1/fragmentsintro/MainActivity.java
+++ Practicals/FragmentsIntro/app/src/main/java/bit/keigdl1/fragmentsintro/MainActivity.java
@@ -1,7 +1,13 @@
 package bit.keigdl1.fragmentsintro;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get ref to buttons
+        Button btnImageFrag = (Button) findViewById(R.id.btnImageFragment);
+        Button btnListViewFrag = (Button) findViewById(R.id.btnListFragment);
+
+        //Bind a custom click handler to imagefrag
+        btnImageFrag.setOnClickListener(new onImgBtnClickHandler());
+    }
+
+    public class onImgBtnClickHandler implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v){
+            //Create an instance of my fragment class
+            Fragment dynamicFragment = new imageViewFragment();
+            android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+
+            //Begin fragment transaction
+            //Use the fragment manager to begin a transition.
+            android.support.v4.app.FragmentTransaction ft = fm.beginTransaction();
+
+            //Replace the placeholder with the fragment
+            ft.replace(R.id.fragHolder, dynamicFragment);
+
+            //Complete the transaction
+            ft.commit();
+        }
     }
 }
