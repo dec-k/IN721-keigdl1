ffc604b5189e5ba2d22130c17c2daea1cb3c051b
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-16 15:29:46 +1300

    Task 2 Complete (Please read desc tho)
    Task complete, and I understand conceptually whats happening aside from
    1 thing. In onActivityResult, when getIntExtra is called, what is the
    significance of the 0 arg? I can't explain it succinctly in my head but
    I understand that it is there to bind the two activities together?

--- Practicals/TextColourPassing/app/src/main/java/bit/keigdl1/textcolourpassing/FirstActivity.java
+++ Practicals/TextColourPassing/app/src/main/java/bit/keigdl1/textcolourpassing/FirstActivity.java
@@ -1,7 +1,11 @@
 package bit.keigdl1.textcolourpassing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -11,6 +15,32 @@ public class FirstActivity extends AppCompatActivity {
         setContentView(R.layout.activity_first);
 
         //ref btn
-        
+        Button btnSettings = (Button) findViewById(R.id.btnChangeColour);
+        //bind
+        btnSettings.setOnClickListener(new StartForResultHandler());
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == 0){
+            //ref textview
+            TextView txtColour = (TextView) findViewById(R.id.txtIpsum);
+
+            //get int result
+            int result = data.getIntExtra("colourHex",0);
+            txtColour.setTextColor(result);
+        }
+    }
+
+    public class StartForResultHandler implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            //Setup intent
+            Intent resultIntent = new Intent(FirstActivity.this,SecondActivity.class);
+            //Start an activity which is only instantiated for a result, the activity is never shown on-screen.
+            startActivityForResult(resultIntent,0);
+        }
+    }
+
+
 }
--- Practicals/TextColourPassing/app/src/main/java/bit/keigdl1/textcolourpassing/SecondActivity.java
+++ Practicals/TextColourPassing/app/src/main/java/bit/keigdl1/textcolourpassing/SecondActivity.java
@@ -1,7 +1,10 @@
 package bit.keigdl1.textcolourpassing;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TextView txtCol = (TextView) findViewById(R.id.textView);
+        //Save text colour as int
+        int hexColour = txtCol.getCurrentTextColor();
+
+        //Create an intent, pops activity stack.
+        Intent returnIntent = new Intent();
+
+        //Load the data you want to be returned
+        returnIntent.putExtra("colourHex", hexColour);
+        setResult(Activity.RESULT_OK, returnIntent);
+
+        //Finish processing the intent change
+        finish();
     }
 }
