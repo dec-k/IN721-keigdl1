439fe3903779c4040075521a82cece63ff93fa01
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-16 14:49:10 +1300

    Task 2 Skeleton

---
+++ Practicals/TextColourPassing/app/src/main/java/bit/keigdl1/textcolourpassing/SecondActivity.java
@@ -0,0 +1,13 @@
+package bit.keigdl1.textcolourpassing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
