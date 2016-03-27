3c60b6fae290cfaf83918eb15b1348bdbf0923f1
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-28 10:52:13 +1300

    Added functional click handler to launchAct

--- Practicals/NounQuiz/app/src/main/java/bit/keigdl1/nounquiz/LaunchActivity.java
+++ Practicals/NounQuiz/app/src/main/java/bit/keigdl1/nounquiz/LaunchActivity.java
@@ -1,7 +1,10 @@
 package bit.keigdl1.nounquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LaunchActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        //Set ref to button
+        Button goQuestionBtn = (Button) findViewById(R.id.btnStart);
+        //bind btn to control
+        goQuestionBtn.setOnClickListener(new btnClickHandler());
+    }
+
+    public class btnClickHandler implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            Intent goMainIntent = new Intent(LaunchActivity.this,QuestionAct.class);
+            startActivity(goMainIntent);
+        }
     }
 }
