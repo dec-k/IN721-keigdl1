5d76fc154873bfa87940f4eebbae97fb7ae7e0b4
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-28 09:23:19 +1300

    Initial Noun Quiz Commit

---
+++ Practicals/NounQuiz/app/src/main/java/bit/keigdl1/nounquiz/LaunchActivity.java
@@ -0,0 +1,13 @@
+package bit.keigdl1.nounquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
