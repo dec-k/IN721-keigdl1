bf6027cb321a5de72ea1d81dbd75133c0dd641cb
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-28 10:39:21 +1300

    Question Activity Fixes + Load question onCreate

--- Practicals/NounQuiz/app/src/main/java/bit/keigdl1/nounquiz/QuestionAct.java
+++ Practicals/NounQuiz/app/src/main/java/bit/keigdl1/nounquiz/QuestionAct.java
@@ -3,6 +3,7 @@ package bit.keigdl1.nounquiz;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +12,10 @@ import java.util.Random;
 public class QuestionAct extends AppCompatActivity {
     //Declare a list for the questions to be held in
     List questionList;
+    //Declare a variable to store the score of the user so far
+    int scoreTotal;
+    //Variable for which question the quiz is currently on
+    int currentlyOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,16 @@ public class QuestionAct extends AppCompatActivity {
 
         //Declare & instantiate a question list
         questionList = new ArrayList();
+
+        //Initialise currentlyOn and score
+        scoreTotal = 0;
+        currentlyOn = 0;
+
+        //Run genQuestions so the question list gets loaded and shuffled correctly
+        genQuestions();
+
+        //Load in a question at activity load
+        loadNextQuestion();
     }
 
     public void genQuestions(){
@@ -33,8 +48,8 @@ public class QuestionAct extends AppCompatActivity {
         //Die - Feminine
         questionList.add(new Question("Ente","Die",BitmapFactory.decodeFile(String.valueOf(R.drawable.die_ente))));
         questionList.add(new Question("Hexe","Die",BitmapFactory.decodeFile(String.valueOf(R.drawable.die_hexe))));
-        questionList.add(new Question("Kuh","Die",BitmapFactory.decodeFile(String.valueOf(R.drawable.die_kuh))));
-        questionList.add(new Question("Milch","Die",BitmapFactory.decodeFile(String.valueOf(R.drawable.die_milch))));
+        questionList.add(new Question("Kuh", "Die", BitmapFactory.decodeFile(String.valueOf(R.drawable.die_kuh))));
+        questionList.add(new Question("Milch", "Die", BitmapFactory.decodeFile(String.valueOf(R.drawable.die_milch))));
         questionList.add(new Question("Strasse","Die",BitmapFactory.decodeFile(String.valueOf(R.drawable.die_strasse))));
 
 
@@ -49,12 +64,12 @@ public class QuestionAct extends AppCompatActivity {
     public void shuffleList(){
         //Setup random numbers which align with index values in the question list.
         Random rndShuffle = new Random();
-        int randA = rndShuffle.nextInt(11);
-        int randB = rndShuffle.nextInt(11);
+        int randA = rndShuffle.nextInt(12);
+        int randB = rndShuffle.nextInt(12);
 
         //Prevent the two numbers being the same
         while(randA == randB){
-            randB = rndShuffle.nextInt(11);
+            randB = rndShuffle.nextInt(12);
         }
 
         //Create a temp variable to hold question A
@@ -65,5 +80,16 @@ public class QuestionAct extends AppCompatActivity {
 
         //Add temp back at question B's location.
         questionList.set(randB, temp);
+    }
+
+    public void loadNextQuestion(){
+        //Get references to the required form component
+        ImageView iv = (ImageView)findViewById(R.id.imgQuestion);
+
+        //Set question var for the currentlyOn question
+        Question curOnQuestion = (Question) questionList.get(currentlyOn);
+
+        //Use that questions image accessors to get a bitmap of its image and draw it to the iv
+        iv.setImageBitmap(curOnQuestion.getImage());
     }
 }
