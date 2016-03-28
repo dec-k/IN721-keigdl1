7e6955010adeb934a917eee4fb7ef1eb8666a88f
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-28 18:53:29 +1300

    Added results screen

---
+++ Practicals/NounQuiz/app/src/main/java/bit/keigdl1/nounquiz/ScoreActivity.java
@@ -0,0 +1,13 @@
+package bit.keigdl1.nounquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
+}
