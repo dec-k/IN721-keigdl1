58d339cf9f49bfb8a917c7142bf004688e686866
Dec Keighley <keigdl1@student.op.ac.nz>
2016-04-01 14:35:10 +1300

    Created stubs for custom adapter

--- Practicals/AboutCustomAdapter/app/src/main/java/bit/keigdl1/aboutdunedin/CustomAdapter.java
+++ Practicals/AboutCustomAdapter/app/src/main/java/bit/keigdl1/aboutdunedin/CustomAdapter.java
@@ -1,7 +1,14 @@
 package bit.keigdl1.aboutdunedin;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
 /**
  * Created by keigdl1 on 1/04/2016.
  */
-public class CustomAdapter {
+public class CustomAdapter extends ArrayAdapter<FunThing>{
+
+    public CustomAdapter(Context context, int resource, FunThing[] objects) {
+        super(context, resource, objects);
+    }
 }
