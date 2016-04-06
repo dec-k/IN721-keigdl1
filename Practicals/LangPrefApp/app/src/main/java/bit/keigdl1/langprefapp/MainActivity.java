536b9981fec00c4a3ff73b4ccc8ab1bd2445e6a5
Dec Keighley <keigdl1@student.op.ac.nz>
2016-04-06 15:13:45 +1200

    Practical 1 Complete

--- Practicals/LangPrefApp/app/src/main/java/bit/keigdl1/langprefapp/MainActivity.java
+++ Practicals/LangPrefApp/app/src/main/java/bit/keigdl1/langprefapp/MainActivity.java
@@ -1,12 +1,15 @@
 package bit.keigdl1.langprefapp;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     //Attributes
@@ -21,6 +24,26 @@ public class MainActivity extends AppCompatActivity {
         //If a shared pref doesnt exist, it is created.
         prefs = getSharedPreferences("sharedPrefs",MODE_PRIVATE);
         prefsEditor = prefs.edit();
+
+        //ref button + bind
+        Button btnUpdatePrefs = (Button)findViewById(R.id.btnSetPref);
+        btnUpdatePrefs.setOnClickListener(new SetPrefsClickHandler());
+
+        //fetched stored lang
+        //returns null if kvp not set
+        String languagePreference = prefs.getString("language",null);
+        String colourPref = prefs.getString("colour",null);
+        if(languagePreference!=null){
+            String greetingInChosenLanguage = updateActivity(languagePreference);
+
+            //update textview
+            TextView greeting = (TextView)findViewById(R.id.txtHello);
+            greeting.setText(greetingInChosenLanguage);
+
+        }
+        if(colourPref!=null){
+            updateColour(colourPref);
+        }
     }
 
     public class SetPrefsClickHandler implements View.OnClickListener{
@@ -43,6 +66,43 @@ public class MainActivity extends AppCompatActivity {
             //store chosen colour too
             prefsEditor.putString("colour",checkedColour);
             prefsEditor.commit();
+
+            //update textview
+            TextView greeting = (TextView)findViewById(R.id.txtHello);
+            greeting.setText(updateActivity(checkedLanguage));
+            updateColour(checkedColour);
+        }
+    }
+
+    public String updateActivity(String language){
+        String greeting = "";
+        switch(language){
+            case("French"):
+                greeting = "Hon Hon Hon";
+                break;
+            case("German"):
+                greeting = "Hallo Welt";
+                break;
+            case("Spanish"):
+                greeting = "Hola Mundo";
+                break;
+        }
+
+        return greeting;
+    }
+
+    public void updateColour(String colour){
+        TextView greeting = (TextView)findViewById(R.id.txtHello);
+        switch(colour){
+            case("Red"):
+                greeting.setTextColor(Color.RED);
+                break;
+            case("Blue"):
+                greeting.setTextColor(Color.BLUE);
+                break;
+            case("Green"):
+                greeting.setTextColor(Color.GREEN);
+                break;
         }
     }
 }
