68398cfc0e6250bfff83db592d664511ea87df46
Dec Keighley <keigdl1@student.op.ac.nz>
2016-05-05 21:22:34 +1200

    Began writing async task to fetch nearest city

--- Practicals/TeleporterTour/app/src/main/java/bit/keigdl1/teleportertour/MainActivity.java
+++ Practicals/TeleporterTour/app/src/main/java/bit/keigdl1/teleportertour/MainActivity.java
@@ -1,11 +1,13 @@
 package bit.keigdl1.teleportertour;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.Random;
 
@@ -47,12 +49,48 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View v) {
+
+            //Setup async processing to get nearby city and display.
+            AsyncFetchNearestCity APIThread = new AsyncFetchNearestCity();
+
+            //Execute async call
+            APIThread.execute();
+
+            /*
             //Generate a lat and a lng, then feed them to a displayer method.
             double lat = genLocParameter(90);
             double lng = genLocParameter(180);
 
             //Populate both fields on the form.
             populateLatLng(lat, lng);
+            */
+        }
+    }
+
+    class AsyncFetchNearestCity extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            //Generate lat and lng vals
+            double lat = genLocParameter(90);
+            double lng = genLocParameter(180);
+
+            try {
+
+                //Url of where we retrieve json data from.
+                String requestURL = "http://www.geoplugin.net/extras/location.gp?" +
+                        "lat=" + lat +
+                        "&long=" + lng +
+                        "&format=json";
+
+                //Create a url object
+                URL URLObject = new URL(requestURL);
+
+            }
+            catch(Exception e){e.printStackTrace();}
+
+
+            return null;
         }
     }
 }
