11bf0b89dc77ce2a2a20df128ebf4237746a8098
Dec Keighley <keigdl1@student.op.ac.nz>
2016-05-09 23:45:10 +1200

    GPS Polling Not functioning :cry:

--- Practicals/TeleporterTour/app/src/main/java/bit/keigdl1/teleportertour/MainActivity.java
+++ Practicals/TeleporterTour/app/src/main/java/bit/keigdl1/teleportertour/MainActivity.java
@@ -1,14 +1,18 @@
 package bit.keigdl1.teleportertour;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.media.Image;
 import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -39,8 +43,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Ref and bind btn
-        Button btnTele = (Button)findViewById(R.id.btnTele);
+        Button btnTele = (Button) findViewById(R.id.btnTele);
         btnTele.setOnClickListener(new teleportClickHandler());
+
+        //Loc manager setup
+        //Create a location manager instance
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //Create a criteria instance, criteria types hold criterium for determining the GPS method to use.
+        Criteria cri = new Criteria();
+        //Set provider info
+        String providerName = lm.getBestProvider(cri, false);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        lm.requestLocationUpdates(providerName, 400, 1, new customLocListener());
+
+        /*Set a last known location
+        Location loc = lm.getLastKnownLocation(providerName);*/
     }
 
     private double genLocParameter(double param){
@@ -53,6 +80,31 @@ public class MainActivity extends AppCompatActivity {
         double latlngCalc = ((param * 2) * genVal) - param;
 
         return latlngCalc;
+    }
+
+    private class customLocListener implements LocationListener{
+        @Override
+        public void onLocationChanged(Location location){
+            //Setup async processing to get nearby city and display.
+            AsyncFetchNearestCityNoFail APIThread = new AsyncFetchNearestCityNoFail(MainActivity.this);
+
+            //Execute async call
+            APIThread.execute();
+        }
+
+        //Other methods not used
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
     }
 
     private void populateLatLng(double lat, double lng){
