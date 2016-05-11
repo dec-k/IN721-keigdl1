7a0bdb7d375c637cc858db891e28dee1f796eff8
Dec Keighley <keigdl1@student.op.ac.nz>
2016-05-11 14:21:07 +1200

    Wrote method to generate image file

--- Practicals/Mosaic/app/src/main/java/bit/keigdl1/mosaic/MainActivity.java
+++ Practicals/Mosaic/app/src/main/java/bit/keigdl1/mosaic/MainActivity.java
@@ -1,7 +1,13 @@
 package bit.keigdl1.mosaic;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     //Class variable for camera image that gets taken
@@ -11,6 +17,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private File makePhotoFile(){
+        //System image folder
+        File imageRootPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        //Make subdirectory
+        File imageStorageDirectory = new File(imageRootPath, "MosaicSnaps");
+        if(!imageStorageDirectory.exists()){
+            imageStorageDirectory.mkdirs();
+        }
+
+        //Get timestamp
+        SimpleDateFormat timeStampFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        Date currentTime = new Date();
+        String timeStamp = timeStampFormat.format(currentTime);
+
+        mPhotoFileName = "IMG_" + timeStamp + ".jpg";
+
+        File photoFile = new File(imageStorageDirectory.getPath() + File.separator + mPhotoFileName);
+        return photoFile;
+    }
+
+    private class customClick implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            
+        }
     }
 
 
