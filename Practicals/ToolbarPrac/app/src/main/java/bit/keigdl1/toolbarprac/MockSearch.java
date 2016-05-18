a5d685af4b6cf00b9f0ca4eea08d5353c7ae2324
Dec Keighley <keigdl1@student.op.ac.nz>
2016-05-18 17:34:46 +1200

    Added menu interaction to other activites

--- Practicals/ToolbarPrac/app/src/main/java/bit/keigdl1/toolbarprac/MockSearch.java
+++ Practicals/ToolbarPrac/app/src/main/java/bit/keigdl1/toolbarprac/MockSearch.java
@@ -1,9 +1,11 @@
 package bit.keigdl1.toolbarprac;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MockSearch extends AppCompatActivity {
 
@@ -19,5 +21,37 @@ public class MockSearch extends AppCompatActivity {
         menuInflater.inflate(R.menu.custom_menu, menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        //Decided to create variables here, since I'm gonna need them in 3 seperate places.
+        String itemTitle = (String) menuItem.getTitle();
+
+        //Declare intent
+        Intent intent;
+
+        switch(itemTitle){
+            case "Home":
+                //Create intent
+                intent = new Intent(this, MainActivity.class);
+                //Start activity
+                startActivity(intent);
+                break;
+            case "Settings":
+                //Create intent
+                intent = new Intent(this, MockSettings.class);
+                //Start activity
+                startActivity(intent);
+                break;
+            case "Search":
+                //Create intent
+                intent = new Intent(this, MockSearch.class);
+                //Start activity
+                startActivity(intent);
+                break;
+        }
+
+        //Return indicator that the method has finished processing. This is not an indicator of a fail-state.
+        return true;
     }
 }
