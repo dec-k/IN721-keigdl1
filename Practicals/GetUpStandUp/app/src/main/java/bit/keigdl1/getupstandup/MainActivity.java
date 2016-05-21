59ae27524b26dcffa81b62a8a172e134ffcd3e76
Dec Keighley <keigdl1@student.op.ac.nz>
2016-05-21 13:04:04 +1200

    Practical 3 Finished

--- Practicals/GetUpStandUp/app/src/main/java/bit/keigdl1/getupstandup/MainActivity.java
+++ Practicals/GetUpStandUp/app/src/main/java/bit/keigdl1/getupstandup/MainActivity.java
@@ -2,12 +2,40 @@ package bit.keigdl1.getupstandup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView img;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setup refs
+        img = (ImageView)findViewById(R.id.imageView);
+        btn = (Button)findViewById(R.id.btnAnimate);
+
+        //Bind
+        btn.setOnClickListener(new animateClickHandler());
+    }
+
+    public class animateClickHandler implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            //Set image here so it's invisible at load.
+            img.setImageResource(R.drawable.cough);
+
+            //Start animating
+            YoYo.with(Techniques.StandUp)
+                    .duration(500)
+                    .playOn(img);
+        }
     }
 }
